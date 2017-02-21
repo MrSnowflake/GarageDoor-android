504ee13fa4c0b8076332a5ddca2f1a6f44238769
Maarten Krijn <Maarten.Krijn@amplexor.com>
2017-02-21 21:21:29 +0100

    Added example settings java file

---
+++ app/src/main/java/com/deepfrozendevelopers/garagedoor/Settings_Example.java
@@ -0,0 +1,9 @@
+package com.deepfrozendevelopers.garagedoor;
+
+/**
+ * Created by maarten.krijn on 2017/02/21.
+ */
+
+public class Settings_Example {
+	public static final String SHA_KEY = "YourArduinoShaKeyHere";
+}
